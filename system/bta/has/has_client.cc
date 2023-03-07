miyu-xu/packages-modules-Bluetooth

gatt: Prepare GATTC_OPEN API for more than direct and background
In following patch there will be added another connection type.
This patch just prepares for that.

Bug: 248340126
Test: atest BluetoothInstrumentationTests
Tag: #feature
Change-Id: Iea86534687dc5f0febc4a9c493e011750ebf1823

system/bta/has/has_client.cc
@@ -166,11 +166,12 @@ class HasClientImpl : public HasClient {
                                  HasDevice::MatchAddress(addr));
       if (device == devices_.end()) {
         devices_.emplace_back(addr, true);
-        BTA_GATTC_Open(gatt_if_, addr, true, false);
+        BTA_GATTC_Open(gatt_if_, addr, BTA_GATT_DIRECT_CONNECTION, false);
 
       } else {
         device->is_connecting_actively = true;
-        if (!device->IsConnected()) BTA_GATTC_Open(gatt_if_, addr, true, false);
+        if (!device->IsConnected())
+          BTA_GATTC_Open(gatt_if_, addr, BTA_GATT_DIRECT_CONNECTION, false);
       }
     }
   }
@@ -190,7 +191,7 @@ class HasClientImpl : public HasClient {
         devices_.push_back(HasDevice(address, features));
 
       /* Connect in background */
-      BTA_GATTC_Open(gatt_if_, address, false, false);
+      BTA_GATTC_Open(gatt_if_, address, BTA_GATT_BKG_CONNECT_ALLOW_LIST, false);
     }
   }
 
@@ -1855,7 +1856,9 @@ class HasClientImpl : public HasClient {
     DoDisconnectCleanUp(*device, peer_disconnected ? false : true);
 
     /* Connect in background - is this ok? */
-    if (peer_disconnected) BTA_GATTC_Open(gatt_if_, device->addr, false, false);
+    if (peer_disconnected)
+      BTA_GATTC_Open(gatt_if_, device->addr, BTA_GATT_BKG_CONNECT_ALLOW_LIST,
+                     false);
   }
 
   void OnGattServiceSearchComplete(const tBTA_GATTC_SEARCH_CMPL& evt) {

system/bta/vc/vc.cc
@@ -104,7 +104,7 @@ class VolumeControlImpl : public VolumeControl {
       device->connecting_actively = true;
     }
 
-    BTA_GATTC_Open(gatt_if_, address, true, false);
+    BTA_GATTC_Open(gatt_if_, address, BTA_GATT_DIRECT_CONNECTION, false);
   }
 
   void AddFromStorage(const RawAddress& address, bool auto_connect) {
@@ -115,7 +115,7 @@ class VolumeControlImpl : public VolumeControl {
       volume_control_devices_.Add(address, false);
 
       /* Add device into BG connection to accept remote initiated connection */
-      BTA_GATTC_Open(gatt_if_, address, false, false);
+      BTA_GATTC_Open(gatt_if_, address, BTA_GATT_BKG_CONNECT_ALLOW_LIST, false);
     }
   }
 
@@ -606,7 +606,8 @@ class VolumeControlImpl : public VolumeControl {
       volume_control_devices_.Add(remote_bda, true);
 
       /* Add device into BG connection to accept remote initiated connection */
-      BTA_GATTC_Open(gatt_if_, remote_bda, false, false);
+      BTA_GATTC_Open(gatt_if_, remote_bda, BTA_GATT_BKG_CONNECT_ALLOW_LIST,
+                     false);
     }
   }
 

omansh-krishn/android_packages_modules_Bluetooth

system/bta/has/has_client.cc
@@ -166,11 +166,12 @@ class HasClientImpl : public HasClient {
                                  HasDevice::MatchAddress(addr));
       if (device == devices_.end()) {
         devices_.emplace_back(addr, true);
-        BTA_GATTC_Open(gatt_if_, addr, true, false);
+        BTA_GATTC_Open(gatt_if_, addr, BTM_BLE_DIRECT_CONNECTION, false);
 
       } else {
         device->is_connecting_actively = true;
-        if (!device->IsConnected()) BTA_GATTC_Open(gatt_if_, addr, true, false);
+        if (!device->IsConnected())
+          BTA_GATTC_Open(gatt_if_, addr, BTM_BLE_DIRECT_CONNECTION, false);
       }
     }
   }
@@ -190,7 +191,7 @@ class HasClientImpl : public HasClient {
         devices_.push_back(HasDevice(address, features));
 
       /* Connect in background */
-      BTA_GATTC_Open(gatt_if_, address, false, false);
+      BTA_GATTC_Open(gatt_if_, address, BTM_BLE_BKG_CONNECT_ALLOW_LIST, false);
     }
   }
 
@@ -1855,7 +1856,9 @@ class HasClientImpl : public HasClient {
     DoDisconnectCleanUp(*device, peer_disconnected ? false : true);
 
     /* Connect in background - is this ok? */
-    if (peer_disconnected) BTA_GATTC_Open(gatt_if_, device->addr, false, false);
+    if (peer_disconnected)
+      BTA_GATTC_Open(gatt_if_, device->addr, BTM_BLE_BKG_CONNECT_ALLOW_LIST,
+                     false);
   }
 
   void OnGattServiceSearchComplete(const tBTA_GATTC_SEARCH_CMPL& evt) {

system/bta/vc/vc.cc
@@ -104,7 +104,7 @@ class VolumeControlImpl : public VolumeControl {
       device->connecting_actively = true;
     }
 
-    BTA_GATTC_Open(gatt_if_, address, true, false);
+    BTA_GATTC_Open(gatt_if_, address, BTM_BLE_DIRECT_CONNECTION, false);
   }
 
   void AddFromStorage(const RawAddress& address, bool auto_connect) {
@@ -115,7 +115,7 @@ class VolumeControlImpl : public VolumeControl {
       volume_control_devices_.Add(address, false);
 
       /* Add device into BG connection to accept remote initiated connection */
-      BTA_GATTC_Open(gatt_if_, address, false, false);
+      BTA_GATTC_Open(gatt_if_, address, BTM_BLE_BKG_CONNECT_ALLOW_LIST, false);
     }
   }
 
@@ -592,7 +592,8 @@ class VolumeControlImpl : public VolumeControl {
       volume_control_devices_.Add(remote_bda, true);
 
       /* Add device into BG connection to accept remote initiated connection */
-      BTA_GATTC_Open(gatt_if_, remote_bda, false, false);
+      BTA_GATTC_Open(gatt_if_, remote_bda, BTM_BLE_BKG_CONNECT_ALLOW_LIST,
+                     false);
     }
   }
 